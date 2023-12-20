90861c7365a1b6c77d8ce31cb59a068c9fd521f3
xiaoxiao <253451518@qq.com>
2023-12-20 18:27:41 +0800

    完成

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+ git push -f git@github.com:xiaoli0510/xiaoli0510.github.io.git master
+
+
+cd -
